7a6e32637c890ae201bd9154fa3ca4336003dc00
azhar467 <azhar.ahmed467@gmail.com>
2021-10-09 16:25:02 +0530

    Migrated to postgres, Upgraded to spring 2.5.5 and upgraded swagger to open api ui.

--- spring-demo/src/main/java/com/azhar/springdemo/controller/BankController.java
+++ spring-demo/src/main/java/com/azhar/springdemo/controller/BankController.java
@@ -3,22 +3,16 @@ package com.azhar.springdemo.controller;
 import com.azhar.springdemo.exception.BankNotFoundException;
 import com.azhar.springdemo.model.Bank;
 import com.azhar.springdemo.service.BankService;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
 
 @RestController
-@RequestMapping(value = "/bank", produces = "application/hal+json")
-@Api(value = "Bank Resource REST End Point",description = "Shows bank Info")
+@RequestMapping(value = "/bank", produces = "application/json")
 public class BankController {
 
     @Autowired
@@ -32,12 +26,6 @@ public class BankController {
 
     @GetMapping("/bankDetails/{id}")
     @ResponseStatus(HttpStatus.OK)
-    @ApiOperation(value = "Returns All Bank Details", notes = "Shows all bank details")
-    @ApiResponses(value = {
-            @ApiResponse(code = 200, message = "The request has succeeded"),
-            @ApiResponse(code = 401, message = "The request requires user authentication"),
-            @ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
-            @ApiResponse(code = 404, message = "The server has not found anything matching the Request-URI")})
     public Optional<Bank> getBankDetails(@PathVariable("id") Integer id){
         return bankService.getBankById(id);
     }
